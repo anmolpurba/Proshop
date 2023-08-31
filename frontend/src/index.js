66c2a91c761b092090eede3da3cb60620a116c83
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-08-31 22:43:56 +0530

    added react-router-dom and configured it. Also added react-router-bootstrap to provide links to navabar components to prevent reloading

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
+import { createBrowserRouter, Route, RouterProvider} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App />,
+    children:[
+      {
+        path:"/",
+        element:<HomeScreen />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
