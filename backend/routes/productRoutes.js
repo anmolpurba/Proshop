fe34d57f6d5b7a50f0877ce866b8d3a53d812865
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-09-02 11:43:15 +0530

    added seeder.js

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+const router = express.Router();
+import products from '../data/products.js';
+import asyncHandler from "express-async-handler"
+
+router.get('/', asyncHandler(async (req,res) => {
+    res.json(products);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+}));
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from "dotenv";
 dotenv.config();
-import products from "./data/products.js"
 const port = process.env.PORT || 5000;
+import productRoutes from "./routes/productRoutes.js"
 
 connectDB(); //connect to mongodb
 
@@ -13,13 +13,8 @@ app.get('/', (req, res) => {
     res.send('API IS RUNNING...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.send(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product);
-});
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
