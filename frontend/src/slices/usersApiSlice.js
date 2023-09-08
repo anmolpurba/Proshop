d61b1b271f260184d0d80335f6b8726508735400
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-09-08 09:18:38 +0530

    update users

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -42,8 +42,22 @@ export const userApiSlice = apiSlice.injectEndpoints({
           url: `${USERS_URL}/${userId}`,
           method: 'DELETE'
         })
-      })
+      }),
+      getUserDetails: builder.query({
+        query: (id) => ({
+          url: `${USERS_URL}/${id}`,
+        }),
+        keepUnusedDataFor: 5,
+      }),
+      updateUser: builder.mutation({
+        query: (data) => ({
+          url: `${USERS_URL}/${data.userId}`,
+          method: 'PUT',
+          body: data,
+        }),
+        invalidatesTags: ['User'],
+      }),
   }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation } = userApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUserMutation, useGetUserDetailsQuery, useUpdateUserMutation } = userApiSlice;
